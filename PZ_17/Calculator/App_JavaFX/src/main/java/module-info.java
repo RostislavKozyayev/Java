f512ca6cf2a0a3a8a6830ec54c33dd87706f0b48
RostislavKozyayev <rossttiislav@gmail.com>
2025-12-14 22:39:33 +0300

    Delete PZ_17/Calculator/App_JavaFX directory

--- PZ_17/Calculator/App_JavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.app_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.app_javafx to javafx.fxml;
-    exports com.example.app_javafx;
-}
